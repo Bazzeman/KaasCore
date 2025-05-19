8f56445068ea731fd13d6958db79e14034c3c024
Bazzeman <110249979+Bazzeman@users.noreply.github.com>
2025-05-19 14:55:07 +0200

    #4 Wrote ConfigManager

--- KaasCore/src/main/java/bas/pennings/kaasCore/KaasCore.java
+++ KaasCore/src/main/java/bas/pennings/kaasCore/KaasCore.java
@@ -1,12 +1,16 @@
 package bas.pennings.kaasCore;
 
+import bas.pennings.kaasCore.config.ConfigManager;
+import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class KaasCore extends JavaPlugin {
+    @Getter private ConfigManager configManager;
 
     @Override
     public void onEnable() {
-
+        configManager = new ConfigManager(this);
+        configManager.loadConfig();
     }
 
     @Override
